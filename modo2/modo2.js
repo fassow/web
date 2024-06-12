e1b47cf410e7baf9fcac8dbc19090cfbd30c61f2
fassow <felipeandresmarchenarojas@gmail.com>
2024-06-11 22:38:47 -0600

    Add files via upload

---
+++ modo2/modo2.js
@@ -0,0 +1,212 @@
+let elements = {
+    "Hidrógeno": {"símbolo": "H", "tipo": "no metal"},
+    "Helio": {"símbolo": "He", "tipo": "gas noble"},
+    "Litio": {"símbolo": "Li", "tipo": "metal"},
+    "Berilio": {"símbolo": "Be", "tipo": "metal"},
+    "Boro": {"símbolo": "B", "tipo": "metaloide"},
+    "Carbono": {"símbolo": "C", "tipo": "no metal"},
+    "Nitrógeno": {"símbolo": "N", "tipo": "no metal"},
+    "Oxígeno": {"símbolo": "O", "tipo": "no metal"},
+    "Flúor": {"símbolo": "F", "tipo": "no metal"},
+    "Neón": {"símbolo": "Ne", "tipo": "gas noble"},
+    "Sodio": {"símbolo": "Na", "tipo": "metal"},
+    "Magnesio": {"símbolo": "Mg", "tipo": "metal"},
+    "Aluminio": {"símbolo": "Al", "tipo": "metal"},
+    "Silicio": {"símbolo": "Si", "tipo": "metaloide"},
+    "Fósforo": {"símbolo": "P", "tipo": "no metal"},
+    "Azufre": {"símbolo": "S", "tipo": "no metal"},
+    "Cloro": {"símbolo": "Cl", "tipo": "no metal"},
+    "Argón": {"símbolo": "Ar", "tipo": "gas noble"},
+    "Potasio": {"símbolo": "K", "tipo": "metal"},
+    "Calcio": {"símbolo": "Ca", "tipo": "metal"},
+    "Escandio": {"símbolo": "Sc", "tipo": "metal"},
+    "Titanio": {"símbolo": "Ti", "tipo": "metal"},
+    "Vanadio": {"símbolo": "V", "tipo": "metal"},
+    "Cromo": {"símbolo": "Cr", "tipo": "metal"},
+    "Manganeso": {"símbolo": "Mn", "tipo": "metal"},
+    "Hierro": {"símbolo": "Fe", "tipo": "metal"},
+    "Cobalto": {"símbolo": "Co", "tipo": "metal"},
+    "Níquel": {"símbolo": "Ni", "tipo": "metal"},
+    "Cobre": {"símbolo": "Cu", "tipo": "metal"},
+    "Zinc": {"símbolo": "Zn", "tipo": "metal"},
+    "Galio": {"símbolo": "Ga", "tipo": "metal"},
+    "Germanio": {"símbolo": "Ge", "tipo": "metaloide"},
+    "Arsénico": {"símbolo": "As", "tipo": "metaloide"},
+    "Selenio": {"símbolo": "Se", "tipo": "no metal"},
+    "Bromo": {"símbolo": "Br", "tipo": "no metal"},
+    "Kriptón": {"símbolo": "Kr", "tipo": "gas noble"},
+    "Rubidio": {"símbolo": "Rb", "tipo": "metal"},
+    "Estroncio": {"símbolo": "Sr", "tipo": "metal"},
+    "Itrio": {"símbolo": "Y", "tipo": "metal"},
+    "Zirconio": {"símbolo": "Zr", "tipo": "metal"},
+    "Niobio": {"símbolo": "Nb", "tipo": "metal"},
+    "Molibdeno": {"símbolo": "Mo", "tipo": "metal"},
+    "Tecnecio": {"símbolo": "Tc", "tipo": "metal"},
+    "Rutenio": {"símbolo": "Ru", "tipo": "metal"},
+    "Rodio": {"símbolo": "Rh", "tipo": "metal"},
+    "Paladio": {"símbolo": "Pd", "tipo": "metal"},
+    "Plata": {"símbolo": "Ag", "tipo": "metal"},
+    "Cadmio": {"símbolo": "Cd", "tipo": "metal"},
+    "Indio": {"símbolo": "In", "tipo": "metal"},
+    "Estaño": {"símbolo": "Sn", "tipo": "metal"},
+    "Antimonio": {"símbolo": "Sb", "tipo": "metaloide"},
+    "Telurio": {"símbolo": "Te", "tipo": "metaloide"},
+    "Yodo": {"símbolo": "I", "tipo": "no metal"},
+    "Xenón": {"símbolo": "Xe", "tipo": "gas noble"},
+    "Cesio": {"símbolo": "Cs", "tipo": "metal"},
+    "Bario": {"símbolo": "Ba", "tipo": "metal"},
+    "Lantano": {"símbolo": "La", "tipo": "metal"},
+    "Cerio": {"símbolo": "Ce", "tipo": "metal"},
+    "Praseodimio": {"símbolo": "Pr", "tipo": "metal"},
+    "Neodimio": {"símbolo": "Nd", "tipo": "metal"},
+    "Prometio": {"símbolo": "Pm", "tipo": "metal"},
+    "Samario": {"símbolo": "Sm", "tipo": "metal"},
+    "Europio": {"símbolo": "Eu", "tipo": "metal"},
+    "Gadolinio": {"símbolo": "Gd", "tipo": "metal"},
+    "Terbio": {"símbolo": "Tb", "tipo": "metal"},
+    "Disprosio": {"símbolo": "Dy", "tipo": "metal"},
+    "Holmio": {"símbolo": "Ho", "tipo": "metal"},
+    "Erbio": {"símbolo": "Er", "tipo": "metal"},
+    "Tulio": {"símbolo": "Tm", "tipo": "metal"},
+    "Iterbio": {"símbolo": "Yb", "tipo": "metal"},
+    "Lutecio": {"símbolo": "Lu", "tipo": "metal"},
+    "Hafnio": {"símbolo": "Hf", "tipo": "metal"},
+    "Tantalio": {"símbolo": "Ta", "tipo": "metal"},
+    "Tungsteno": {"símbolo": "W", "tipo": "metal"},
+    "Renio": {"símbolo": "Re", "tipo": "metal"},
+    "Osmio": {"símbolo": "Os", "tipo": "metal"},
+    "Iridio": {"símbolo": "Ir", "tipo": "metal"},
+    "Platino": {"símbolo": "Pt", "tipo": "metal"},
+    "Oro": {"símbolo": "Au", "tipo": "metal"},
+    "Mercurio": {"símbolo": "Hg", "tipo": "metal"},
+    "Talio": {"símbolo": "Tl", "tipo": "metal"},
+    "Plomo": {"símbolo": "Pb", "tipo": "metal"},
+    "Bismuto": {"símbolo": "Bi", "tipo": "metal"},
+    "Polonio": {"símbolo": "Po", "tipo": "metal"},
+    "Astato": {"símbolo": "At", "tipo": "no metal"},
+    "Radón": {"símbolo": "Rn", "tipo": "gas noble"},
+    "Francio": {"símbolo": "Fr", "tipo": "metal"},
+    "Radio": {"símbolo": "Ra", "tipo": "metal"},
+    "Actinio": {"símbolo": "Ac", "tipo": "metal"},
+    "Torio": {"símbolo": "Th", "tipo": "metal"},
+    "Protactinio": {"símbolo": "Pa", "tipo": "metal"},
+    "Uranio": {"símbolo": "U", "tipo": "metal"},
+    "Neptunio": {"símbolo": "Np", "tipo": "metal"},
+    "Plutonio": {"símbolo": "Pu", "tipo": "metal"},
+    "Americio": {"símbolo": "Am", "tipo": "metal"},
+    "Curio": {"símbolo": "Cm", "tipo": "metal"},
+    "Berkelio": {"símbolo": "Bk", "tipo": "metal"},
+    "Californio": {"símbolo": "Cf", "tipo": "metal"},
+    "Einstenio": {"símbolo": "Es", "tipo": "metal"},
+    "Fermio": {"símbolo": "Fm", "tipo": "metal"},
+    "Mendelevio": {"símbolo": "Md", "tipo": "metal"},
+    "Nobelio": {"símbolo": "No", "tipo": "metal"},
+    "Lawrencio": {"símbolo": "Lr", "tipo": "metal"},
+    "Rutherfordio": {"símbolo": "Rf", "tipo": "metal"},
+    "Dubnio": {"símbolo": "Db", "tipo": "metal"},
+    "Seaborgio": {"símbolo": "Sg", "tipo": "metal"},
+    "Bohrio": {"símbolo": "Bh", "tipo": "metal"},
+    "Hassio": {"símbolo": "Hs", "tipo": "metal"},
+    "Meitnerio": {"símbolo": "Mt", "tipo": "metal"},
+    "Darmstadtio": {"símbolo": "Ds", "tipo": "metal"},
+    "Roentgenio": {"símbolo": "Rg", "tipo": "metal"},
+    "Copernicio": {"símbolo": "Cn", "tipo": "metal"},
+    "Nihonio": {"símbolo": "Nh", "tipo": "metal"},
+    "Flerovio": {"símbolo": "Fl", "tipo": "metal"},
+    "Moscovio": {"símbolo": "Mc", "tipo": "metal"},
+    "Livermorio": {"símbolo": "Lv", "tipo": "metal"},
+    "Tenesino": {"símbolo": "Ts", "tipo": "metal"},
+    "Oganesón": {"símbolo": "Og", "tipo": "gas noble"}
+};
+
+
+let currentElement;
+let score = 0;
+
+function newQuestion() {
+    currentElement = getRandomElement();
+    let symbol = elements[currentElement].símbolo; // Obtiene el símbolo del elemento
+    document.getElementById("element-label").innerText = `${currentElement} (${symbol})`; // Muestra el nombre del elemento con su símbolo entre paréntesis
+
+    let options = [];
+    let correctType = elements[currentElement].tipo;
+    
+    // Asigna las opciones según el tipo de elemento
+    switch (correctType) {
+        case "gas noble":
+            options = ["metaloide", "gas noble", "no metal", "metal"];
+            break;
+        case "no metal":
+            options = ["metaloide", "gas noble", "no metal", "metal"];
+            break;
+        case "metal":
+            options = ["metaloide", "gas noble", "no metal", "metal"];
+            break;
+        case "metaloide":
+            options = ["metaloide", "gas noble", "no metal", "metal"];
+            break;
+        default:
+            break;
+    }
+
+    for (let i = 0; i < 4; i++) {
+        let button = document.getElementById(`button-${i}`);
+        button.innerText = options[i];
+        button.style.backgroundColor = "hsl(197, 100%, 50%)"; // Restaurar el color de fondo del botón
+    }
+}
+
+function getRandomElement() {
+    return Object.keys(elements)[Math.floor(Math.random() * Object.keys(elements).length)];
+}
+
+function getRandomType() {
+    let types = ["metal", "metaloide", "no metal", "gas noble"];
+    return types[Math.floor(Math.random() * types.length)];
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+function checkAnswer(idx) {
+    let selectedElement = document.getElementById(`button-${idx}`);
+    let selectedType = selectedElement.innerText;
+    let correctType = elements[currentElement].tipo;
+
+    if (selectedType === correctType) {
+        score++;
+        selectedElement.style.backgroundColor = "green"; // Cambiar a verde si es correcto
+        document.getElementById("score").innerText = `Puntaje: ${score}`; // Actualizar el puntaje en la interfaz
+    } else {
+        selectedElement.style.backgroundColor = "red"; // Cambiar a rojo si es incorrecto
+        for (let i = 0; i < 4; i++) {
+            if (elements[currentElement].tipo === document.getElementById(`button-${i}`).innerText) {
+                document.getElementById(`button-${i}`).style.backgroundColor = "#FFB900"; // Marcar la respuesta correcta
+                break;
+            }
+        }
+    }
+    // Reiniciar los colores de fondo después de un breve tiempo
+    setTimeout(() => {
+        for (let i = 0; i < 4; i++) {
+            document.getElementById(`button-${i}`).style.backgroundColor = "hsl(197, 100%, 50%)";
+        }
+        newQuestion();
+    }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    newQuestion();
+    document.getElementById("score").innerText = `Puntaje: ${score}`; // Mostrar el puntaje inicial en la interfaz
+    document.getElementById("exit-button").addEventListener("click", () => {
+        // Acción para salir del juego, puede ser redirigir a otra página o cerrar la ventana
+        window.location.href = 'file:///C:/Users/felip/OneDrive/Documentos/VS/elementosJS/interfaz/index.html'; // Redirigir a la página principal
+    });
+});
+for (let i = 0; i < 4; i++) {
+    document.getElementById(`button-${i}`).addEventListener("click", () => checkAnswer(i));
+}
---
+++ modo4/modo4.js
@@ -0,0 +1,182 @@
+const questions = [
+  {
+    question: 'Quien creó la tabla periodica?',
+    answers: [
+      { text: 'Robert Boyle', correct: false },
+      { text: 'Antoine Lavoisier', correct: false },
+      { text: 'Emanuel Ramirez Calderon', correct: false },
+      { text: 'Dmitri Ivánovich Mendeléiev', correct: true }
+    ]
+  },
+  {
+    question: '¿Quién descubrió el nitrógeno?',
+    answers: [
+      { text: 'Joseph Priestley', correct: false },
+      { text: 'Carl Wilhelm Scheele', correct: false },
+      { text: 'Daniel Rutherford', correct: true },
+      { text: 'Antoine Lavoisier', correct: false }
+    ]
+  },
+  {
+    question: '¿Qué significa el nombre "oxígeno"?',
+    answers: [
+      { text: 'Generador de vida', correct: false },
+      { text: 'Formador de ácidos', correct: true },
+      { text: 'Aire puro', correct: false },
+      { text: 'Gas vital', correct: false }
+    ]
+  },
+  {
+    question: '¿En qué año se descubrió el sodio?',
+    answers: [
+      { text: '1902', correct: false },
+      { text: '1825', correct: false },
+      { text: '1783', correct: false },
+      { text: '1807', correct: true }
+    ]
+  },
+
+  {
+    question: '¿Quién descubrió el carbono?',
+    answers: [
+      { text: 'Antoine Lavoisier', correct: false },
+      { text: 'Joseph Black', correct: true },
+      { text: 'Henry Cavendish', correct: false },
+      { text: 'Robert Hooke', correct: false }
+    ]
+  },
+
+  {
+    question: '¿Qué elemento químico es esencial para la vida en la Tierra?',
+    answers: [
+      { text: 'Hidrógeno', correct: false },
+      { text: 'Nitrógeno', correct: false },
+      { text: 'Carbono', correct: false },
+      { text: 'Oxígeno', correct: true }
+    ]
+  },
+  {
+    question: '¿Quién descubrió la penicilina?',
+    answers: [
+      { text: 'Marie Curie', correct: false },
+      { text: 'Alexander Fleming', correct: true },
+      { text: 'Louis Pasteur', correct: false },
+      { text: 'Robert Koch', correct: false }
+    ]
+  },
+  {
+    question: '¿Cuál es el símbolo químico del oro?',
+    answers: [
+      { text: 'Fe', correct: false },
+      { text: 'Ag', correct: false },
+      { text: 'Au', correct: true },
+      { text: 'Cu', correct: false }
+    ]
+  },
+  {
+    question: '¿Quién formuló la teoría de la relatividad?',
+    answers: [
+      { text: 'Isaac Newton', correct: false },
+      { text: 'Albert Einstein', correct: true },
+      { text: 'Stephen Hawking', correct: false },
+      { text: 'Galileo Galilei', correct: false }
+    ]
+  },
+  {
+    question: '¿Quién descubrió el oxígeno?',
+    answers: [
+      { text: 'Joseph Priestley', correct: true },
+      { text: 'Dmitri Mendeléyev', correct: false },
+      { text: 'Marie Curie', correct: false },
+      { text: 'Isaac Newton', correct: false }
+    ]
+  },
+  {
+    question: '¿En qué año se descubrió el helio?',
+    answers: [
+      { text: '1901', correct: false },
+      { text: '1895', correct: false },
+      { text: '1868', correct: true },
+      { text: '1812', correct: false }
+    ]
+  },
+  {
+    question: '¿Qué significa el nombre "plomo"?',
+    answers: [
+      { text: 'Metal liviano', correct: false },
+      { text: 'Agua', correct: false },
+      { text: 'Líquido', correct: false },
+      { text: 'Metal pesado en latín', correct: true }
+    ]
+  },
+  {
+    question: '¿Quién descubrió el radio?',
+    answers: [
+      { text: 'Marie Curie', correct: true },
+      { text: 'Albert Einstein', correct: false },
+      { text: 'Niels Bohr', correct: false },
+      { text: 'Galileo Galilei', correct: false }
+    ]
+  }
+];
+
+const elementName = document.getElementById('element-name');
+const buttons = document.querySelectorAll('.button');
+const scoreDisplay = document.getElementById('score');
+const exitButton = document.getElementById('exit-button');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startGame() {
+  currentQuestionIndex = 0; // Empieza desde la primera pregunta
+  showQuestion();
+}
+
+function showQuestion() {
+  if (currentQuestionIndex >= questions.length) {
+    window.location.href = 'file:///C:/Users/felip/OneDrive/Documentos/VS/elementosJS/interfaz/index.html'; // Redirigir a la página principal cuando se completen todas las preguntas
+    return;
+  }
+  const question = questions[currentQuestionIndex];
+  elementName.innerText = question.question;
+  question.answers.forEach((answer, index) => {
+    buttons[index].innerText = answer.text;
+    buttons[index].onclick = () => selectAnswer(answer.correct, index);
+    buttons[index].style.backgroundColor = "hsl(197, 100%, 50%)"; // Reiniciar el color de fondo de los botones
+  });
+}
+
+function selectAnswer(correct, index) {
+  if (correct) {
+    score++;
+    buttons[index].style.backgroundColor = "green"; // Cambiar a verde si es correcto
+  } else {
+    buttons[index].style.backgroundColor = "red"; // Cambiar a rojo si es incorrecto
+    // Encontrar el índice de la respuesta correcta
+    const correctIndex = questions[currentQuestionIndex].answers.findIndex(answer => answer.correct);
+    buttons[correctIndex].style.backgroundColor = "#FFB900"; // Cambiar a amarillo la respuesta correcta
+  }
+  setTimeout(() => {
+    currentQuestionIndex++; // Avanzar a la siguiente pregunta
+    showQuestion();
+  }, 1000);
+  updateScore();
+}
+
+function updateScore() {
+  scoreDisplay.innerText = `Puntaje: ${score}`;
+}
+
+function resetGame() {
+  score = 0;
+  updateScore();
+  startGame();
+}
+
+startGame();
+
+document.getElementById("exit-button").addEventListener("click", () => {
+  // Acción para salir del juego, puede ser redirigir a otra página o cerrar la ventana
+  window.location.href = 'file:///C:/Users/felip/OneDrive/Documentos/VS/elementosJS/interfaz/index.html'; // Redirigir a la página principal
+});
